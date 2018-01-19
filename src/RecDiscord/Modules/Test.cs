003192e6e8b264c7943f08db20c10cc73e3c71c1
Carlos Carrera <thewingz@gmail.com>
2018-01-19 19:35:14 +0100

    Add ping command

---
+++ src/RecDiscord/Modules/Test.cs
@@ -0,0 +1,15 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace RecDiscord.Modules
+{
+    [Name("Pruebas")]
+    public class Test : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping"), Summary("Devuelve un pong!")]
+        public async Task PongAsync()
+        {
+            await ReplyAsync($"pong!");
+        }
+    }
+}
